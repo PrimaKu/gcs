9a1289bfe992c5c0d0a98f8a5eff00527880c9ac
Haris Thohir <haris@thohir.com>
2024-06-07 16:58:23 +0700

    feat: upload file

--- gcs_manager.go
+++ gcs_manager.go
@@ -1,5 +1,19 @@
+package gcs
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
 type (
 	GCSManagerInterface interface {
+		UploadFile(bucketName, objectName, filePath string) error
 	}
 
 	GCSManager struct {
@@ -19,4 +33,17 @@ func NewGCSManager(credentialsFile string) (*GCSManager, error) {
 		client: client,
 		ctx:    ctx,
 	}, nil
+}
+
+func (g *GCSManager) UploadFile(bucketName, objectName string, file os.File) error {
+	bucket := g.client.Bucket(bucketName)
+	object := bucket.Object(objectName)
+	writer := object.NewWriter(g.ctx)
+	defer writer.Close()
+
+	if _, err := io.Copy(writer, &file); err != nil {
+		return fmt.Errorf("failed to upload file to GCS: %v", err)
+	}
+
+	return nil
 }
